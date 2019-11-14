0a97f4310b05afc35dc82f168ab6a269c34af9f1
BenLirio <benjlirio@gmail.com>
2019-11-14 10:07:32 -0500

    Update sign-up Sign up now prevents default

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -4,8 +4,13 @@ const onLogin = event => {
   event.preventDefault()
 }
 
+const onSignUp = () => {
+  event.preventDefault()
+}
+
 const addHandlers = () => {
   $('#login').on('submit', onLogin)
+  $('#sign-up').on('submit', onSignUp)
 }
 
 module.exports = {
