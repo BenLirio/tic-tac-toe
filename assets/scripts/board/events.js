c71cae30e37612b962175c154bdf7d42251dc9a5
BenLirio <benjlirio@gmail.com>
2019-11-14 10:07:32 -0500

    JQuery button gave jQuery access to button

--- assets/scripts/board/events.js
+++ assets/scripts/board/events.js
@@ -1,8 +1,14 @@
 'use strict'
 
+const boardPressed = event => {
+  const button = event.target
+  // jQuery now has access to clicked button
+  $('#' + button.id)
+}
+
 const addHandlers = () => {
   for (let i = 0; i < 9; i++) {
-    $('#board-' + i).on('click', console.log)
+    $('#board-' + i).on('click', boardPressed)
   }
 }
 
