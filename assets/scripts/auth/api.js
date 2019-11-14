debf83b84a2439dacaa22a4af16ce9dfdac4d1cd
BenLirio <benjlirio@gmail.com>
2019-11-14 16:02:20 -0500

    Add api handlers Added api handlers wihtout any data

--- assets/scripts/auth/api.js
+++ assets/scripts/auth/api.js
@@ -1,5 +1,41 @@
 'use strict'
 
-module.exports = {
+const config = require('../config')
 
+const signUp = formData => {
+  return $.ajax({
+    url: config.apiUrl + '/sign-up',
+    method: 'POST',
+    data: formData
+  })
+}
+
+const signIn = formData => {
+  return $.ajax({
+    url: config.apiUrl + '/sign-in',
+    method: 'POST',
+    data: formData
+  })
+}
+
+const changePassword = formData => {
+  return $.ajax({
+    url: config.apiUrl + '/sign-up',
+    method: 'POST',
+    data: formData
+  })
+}
+
+const signOut = () => {
+  return $.ajax({
+    url: config.apiUrl + '/sign-out',
+    method: 'POST'
+  })
+}
+
+module.exports = {
+  signUp,
+  signIn,
+  changePassword,
+  signOut
 }
--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -1,26 +1,40 @@
 'use strict'
 
-const onLogin = event => {
+const api = require('./api')
+
+const signIn = event => {
   event.preventDefault()
+  api.signIn()
+    .then()
+    .catch()
 }
 
 const onSignUp = () => {
   event.preventDefault()
+  api.signUp()
+    .then()
+    .catch()
 }
 
 const onChangePassword = () => {
   event.preventDefault()
+  api.changePassword()
+    .then()
+    .catch()
 }
 
-const onLogout = () => {
+const signOut = () => {
   event.preventDefault()
+  api.signOut()
+    .then()
+    .catch()
 }
 
 const addHandlers = () => {
-  $('#login').on('submit', onLogin)
+  $('#sign-in').on('submit', signIn)
   $('#sign-up').on('submit', onSignUp)
   $('#change-password').on('submit', onChangePassword)
-  $('#logout').on('submit', onLogout)
+  $('#logout').on('submit', signOut)
 }
 
 module.exports = {
