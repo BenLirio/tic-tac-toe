aca327e5412d2ca064e7e3cc4f7d59889e88ee93
BenLirio <benjlirio@gmail.com>
2019-11-14 10:07:32 -0500

    Change branch changing to working on the bame board ui

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -1,5 +1,28 @@
 'use strict'
 
-module.exports = {
+const setCurrentPage = name => {
+  pages.forEach((key, page) => {
+    console.log(key, page)
+    if (key === name) {
+      page.show()
+    } else {
+      page.hide()
+    }
+  })
+}
 
+const _pages = {}
+const addPage = (name, page) => {
+  pages[name] = page
+}
+
+setUpPages = pages => {
+  for (const page of pages) {
+    _pages
+  }
+}
+
+module.exports = {
+  setCurrentPage,
+  addPage
 }
