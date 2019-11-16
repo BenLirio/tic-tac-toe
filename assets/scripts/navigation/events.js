3bde578521de03367ada8e7366eedde49a155f7a
BenLirio <benjlirio@gmail.com>
2019-11-16 15:57:24 -0500

    Remove unnessesary

--- assets/scripts/navigation/events.js
+++ assets/scripts/navigation/events.js
@@ -1,10 +1,8 @@
 'use strict'
 
-const handler = require('./handler')
 const pages = require('./pages')
 
 const addHandlers = function () {
-  $('.back').on('click', () => handler.goBackPage())
   $('.page').each(pages.addPage)
   $('button[set-page]').on('click', event => pages.setPage(event.target))
   pages.setPage('sign-in')
