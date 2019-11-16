13f2c99c17f36b2304a1f952045b4cb65613033f
BenLirio <benjlirio@gmail.com>
2019-11-16 16:00:49 -0500

    Remove unnessesary event

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,14 +1,12 @@
 'use strict'
 // Require Authorization Events
 const authEvents = require('./auth/events')
-const menuEvents = require('./menu/events')
 const boardEvents = require('./board/events')
 const navigationEvents = require('./navigation/events')
 
 $(() => {
   /* Add Authorization Events */
   authEvents.addHandlers()
-  menuEvents.addHandlers()
   boardEvents.addHandlers()
   navigationEvents.addHandlers()
 })
