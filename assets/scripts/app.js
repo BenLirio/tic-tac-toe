0658ac56713fe65527e5ee6a34dec514b10fa50c
BenLirio <benjlirio@gmail.com>
2019-11-16 22:15:25 -0500

    Create OLD folder created a folder for all of the old files

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,11 +1,12 @@
 'use strict'
-// Require Authorization Events
+
 const authEvents = require('./auth/events')
 const gameEvents = require('./game/events')
 const navEvents = require('./nav/events')
 
 $(() => {
-  /* Add Authorization Events */
+  // First Java Script
+  // Initialize all event handlers once document loads
   authEvents.addHandlers()
   gameEvents.addHandlers()
   navEvents.addHandlers()
