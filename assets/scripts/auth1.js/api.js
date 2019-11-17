40ac000d538bdcb61e0582c0227c00c90d5a3e66
BenLirio <benjlirio@gmail.com>
2019-11-16 22:06:12 -0500

    Add new files

---
+++ assets/scripts/auth1.js/api.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const inter = require('../inter/api')
+
+module.exports = {
+
+}
---
+++ assets/scripts/game/events.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const addHandlers = () => {
+
+}
+
+module.exports = {
+  addHandlers
+}
