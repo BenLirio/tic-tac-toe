c30d25897513764ee8b0ac0618ad593b5e750bc7
BenLirio <benjlirio@gmail.com>
2019-11-18 09:10:24 -0500

    Create navigation pages

--- assets/scripts/nav/events.js
+++ assets/scripts/nav/events.js
@@ -6,6 +6,7 @@ const ui = require('./ui')
 const eventHandler = new EventHandler()
 
 eventHandler.addEvents = function () {
+  ui.showPageById($('.page.first-page')[0].id)
   $('button[data-set-page]').on('click', ui.showPageByEvent)
 }
 
